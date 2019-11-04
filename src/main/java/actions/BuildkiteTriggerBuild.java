407e3d9707d3c195043c152f3a1aaa751d58b1a5
Daniel Niknam - Home <danielniknam-home@Danials-MBP-2.lan>
2019-11-05 00:31:22 +1100

    open configuration if no api key supplied

--- src/main/java/actions/BuildkiteTriggerBuild.java
+++ src/main/java/actions/BuildkiteTriggerBuild.java
@@ -3,14 +3,25 @@ package actions;
 import buildkite.BuildkiteClient;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import settings.BuildkiteSettings;
+import settings.BuildkiteSettingsConfigurable;
 
 import java.io.IOException;
 
 public class BuildkiteTriggerBuild extends AnAction {
 
+    private static BuildkiteSettings buildkiteSettings = ServiceManager.getService(BuildkiteSettings.class);
+
     @Override
     public void actionPerformed(AnActionEvent event) {
+        if (buildkiteSettings.getAccessTokenAPI().equals("")) {
+            ShowSettingsUtil showSettingsUtil = ShowSettingsUtil.getInstance();
+            showSettingsUtil.showSettingsDialog(null, BuildkiteSettingsConfigurable.class);
+            return;
+        }
         BuildkiteClient buildkiteClient = BuildkiteClient.getInstance();
         String message = "";
         try {
